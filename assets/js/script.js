4dc2a76cfc1f29a2753d0ba09b446252891a4f32
Ali Chaghou <email@ali.supercode.gmail.com>
2021-07-22 11:43:02 +0200

    Taschenrechner Projekt

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+function appendOperation(operation) {
+    document.getElementById('ergebnisFläche').innerHTML += operation;
+}
+
+function TaschenrechnerErgebnis() {
+    let container = document.getElementById('ergebnisFläche');
+    let result = eval(container.innerHTML);
+    container.innerHTML = result;
+
+}
+
+function ZuletztsGelöscht() {
+    let container = document.getElementById('ergebnisFläche');
+    if (container.innerHTML.endsWith(' ')) {
+        container.innerHTML = container.innerHTML.slice(0, -3);
+    } else {
+        container.innerHTML = container.innerHTML.slice(0, -1);
+    }
+
+}
